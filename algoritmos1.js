00ca000d9b94a7feb5191b792fef318a52cda983
valenarangot <112649107+valenarangot@users.noreply.github.com>
2023-02-07 08:12:55 -0500

    Add files via upload

---
+++ algoritmos1.js
@@ -0,0 +1,17 @@
+
+
+// In operator
+let myArray = [11];
+myArray[2,5,10] = myArray + "by 3";
+myArray[6,11] = myArray + "by 5";
+
+function add(a = 1, N = 50);
+if add(3 % 3) = "by 3";
+if add(5 % 5) = "by 5";
+if add(15 % 5+3) = "by both";
+else = none
+
+
+
+
+   
